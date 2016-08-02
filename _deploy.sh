def4ea8b9dd07e83e07aeac300491197ee240b41
shuest <zhanshua@163.com>
2016-08-03 00:08:29 +0800

    Revert "aaa"
    This reverts commit 496167de78e8e3e88c30efe29b7537d57bce1a2c.

--- _deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-rsync -avvu --delete-after --delete-excluded _site/ webapp@vec.io:apps/vec.io_jekyll/current/public/
-rsync -avvu --delete-after --delete-excluded _config/ webapp@vec.io:apps/vec.io_jekyll/current/config/
